bf5ff91d9b71ffc594cca1d75744f412b3f7af01
Alli Nilles <anilles137@gmail.com>
2017-03-28 13:13:56 -0500

    build works, tests do not

--- src/Improv.hs
+++ src/Improv.hs
@@ -13,8 +13,10 @@ data Direction = Lef | Righ | Forward
      deriving (Show, Eq)
 
 data Angle = Angle Double
+        deriving (Show, Eq)
 
 data Plane = XY | YZ | XZ
+        deriving (Show, Eq)
 
 class Symmetric a where
     refl :: Plane -> a -> a
@@ -35,6 +37,7 @@ instance Symmetric Angle where
     refl _ (Angle x) = Angle (-x)
 
 data Length = Zero | Quarter | Half | ThreeFourths | Full
+        deriving (Show, Eq)
 
 -- should we have an inherent "rhythm" like Tidal does?
 -- I thinks yes
@@ -44,38 +47,72 @@ type Duration = Double
 data Action = Move Direction -- end effector?
             | Turn Angle -- rotate around axis: how to specify?
             | Support -- physical constraint of not falling over
+        deriving (Show, Eq)
 
 -- super: "to which b is attached"
 -- child: "parts attached to b"
 -- this doesn't make sense to me
 -- we really want a collection of parts to also be a part: contains/containedBy?
 class Parts b where
-    superPart :: b -> [b]
-    childPart :: b -> [b]
     contains :: b -> [b]
     containedBy :: b -> [b]
     symmetricPart :: b -> b
     size :: b -> XYZ
     jointAt :: b -> b -> Maybe (XYZ, XYZ)
 
+-- should we use inductive graphs? yesss
+-- right now is a binary tree with pointer to parent node
+data KineChain a = Link (KineChain a) a
+                 | Joint (KineChain a) (KineChain a) (KineChain a)
+     deriving (Show, Eq)
+
+-- can add more data constructors for different robot representations
+-- ie: graphs, forests, fingertrees
+type Robot a = KineChain a
+
+--instance Parts (Robot a) where
+    --contains (Link _ ln) = ln
+    --contains (Joint _ k1 k2) = (contains k1) ++ (contains k2)
+    --containedBy (Link parent _) = parent ++ (containedBy parent)
+    --containedBy (Joint parent _ _) = parent ++ (containedBy parent)
+    --symmetricPart = ?
+    --size =
+    --jointAt =
+
 type XYZ = (Double, Double, Double)
 
 -- parameterized over parts, but we want to map over actions
+-- every dance should be compilable to hardware
+-- so base case should involve a "Part"
+-- in general I don't like divorcing Actions from Parts
 data Dance b = Prim Action Duration
              | Rest Duration
              | WithPart b (Dance b)
              | Dance b :+: Dance b -- in series
              | Dance b :=: Dance b -- in parallel
+        deriving (Show, Eq)
 
 -- combinators
 --------------
+
+newtype Par a = Par (Dance a) deriving (Show)
+
+instance (Parts a) => Monoid (Par a) where
+    mempty = Par (Rest 0)
+    mappend (Par x) (Par y) = Par (x :=: y)
+
+newtype Seq a = Seq (Dance a) deriving (Show)
+
+instance (Parts a) => Monoid (Seq a) where
+    mempty = Seq (Rest 0)
+    mappend (Seq x) (Seq y) = Seq (x :+: y)
 
 seqL, parL :: (Parts a) => [Dance a] -> Dance a
 seqL = foldr (:+:) (Rest 0)
 parL = foldr (:=:) (Rest 0)
 
-repeatn :: (Parts a) => Int -> Dance a -> [Dance a]
-repeatn n dance = take n $ repeat dance
+repeatn :: (Parts a) => Int -> Dance a -> Dance a
+repeatn n dance = seqL $ take n $ repeat dance
 
 -- transformers
 ---------------
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,10 @@
+import Improv
+
+actions :: [Dance a]
+actions = [Prim (Move Forward) 1.0, Prim (Move Backward) 1.0]
+
+
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+    print $ mconcat dance1
