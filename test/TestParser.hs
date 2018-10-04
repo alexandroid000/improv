f06ada5c72bcd9532da9dbca6d38e2d9e5170560
Alli Nilles <anilles137@gmail.com>
2018-10-04 15:34:12 -0500

    test/TestParser.hs: add multirobot parser test

--- test/TestParser.hs
+++ test/TestParser.hs
@@ -7,6 +7,7 @@ import Text.ParserCombinators.Parsec
 import Parser
 
 f1 = "beat 20\n\nx = left right\n\nturtle1 $ x"
+f2 = "beat 20\n\nx = left right\n\nturtle1 turtle2 $ x"
 
 parser_tests = describe "Parser.hs" $ do
         it "basic parse" $
@@ -15,3 +16,7 @@ parser_tests = describe "Parser.hs" $ do
             Right (Node [Leaf "20",Node [Leaf "=",Leaf "x",Node [Leaf
             "left",Leaf "right"]],Node [Leaf "$",Node [Leaf "turtle1"],Node
             [Leaf "x"]]])
+        it "multirob parse" $
+            parse parseDoc "" f2
+            `shouldBe`
+            Right (Node [Leaf "20",Node [Leaf "=",Leaf "x",Node [Leaf "left",Leaf "right"]],Node [Leaf "$",Node [Leaf "turtle1",Leaf "turtle2"],Node [Leaf "x"]]])
