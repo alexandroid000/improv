c883257470dc35ad829a883a77cb128b9d3e9c73
Alli Nilles <anilles137@gmail.com>
2019-01-28 18:11:42 -0600

    make switching between simulators slightly easier

--- app/Main.hs
+++ app/Main.hs
@@ -3,29 +3,36 @@
 import Ros.Node
 import Ros.Topic.Util (topicRate)
 import Ros.Geometry_msgs.Twist as Twist
---import Data.Default.Generics (def)
---import Lens.Family ((.~), (&))
 
 import Parser
 import RobotSpec
---import qualified Control.Exception as E
 import System.Environment
 import qualified Data.Map as Map
 import Ros.Turtlesim.Pose as P
 import Lens.Family (view)
 
--- robotRate, moveTopic should be defined in RobotSpec module
 
-showMsg :: P.Pose -> IO ((Float,Float))
-showMsg pose = return (P._x pose, P._y pose)
+data Engine = TurtleSim | Gazebo
 
+engine = Gazebo
+fname = "./user_instructions.txt"
+
+main :: IO ()
+main = do
+    doc <- readFile fname
+    case convertFile doc of
+        Right moveTopics -> runNode "HaskellTurtle" $ do
+            case engine of
+                TurtleSim -> mapM (\(name, twist) -> advertise (name ++ "/cmd_vel") (topicRate robotRes twist)) $ Map.toList moveTopics
+                Gazebo -> mapM (\(name, twist) -> advertise "/cmd_vel_mux/input/teleop" (topicRate robotRes twist)) $ Map.toList moveTopics
+        Left (ParseErr (-1) err) -> putStrLn $ "Parse error " ++ err
+        Left (ParseErr line err) -> putStrLn $ "Error (line " ++ (show line) ++ "):\n" ++ err
+
+
+
+-- not being used right now, keeping as example of how to access pose
 get_pose :: Node ((Topic IO) Twist)
 get_pose = subscribe "/turtle1/pose"
 
-main :: IO ()
-main = do doc <- readFile "./user_instructions.txt"
-          case convertFile doc of 
-               Right moveTopics -> runNode "HaskellTurtle" $
-                     do mapM (\(name, twist) -> advertise "/cmd_vel_mux/input/teleop" (topicRate robotRes twist)) $ Map.toList moveTopics
-               Left (ParseErr (-1) err) -> putStrLn $ "Parse error " ++ err
-               Left (ParseErr line err) -> putStrLn $ "Error (line " ++ (show line) ++ "):\n" ++ err
+showMsg :: P.Pose -> IO ((Float,Float))
+showMsg pose = return (P._x pose, P._y pose)
--- improv.sh
+++ improv.sh
@@ -1,8 +1,18 @@
 #! /usr/bin/env bash
 
+
+export TURTLE=turtle.launch
+export GAZEBO=turtlebot_world.launch
 export IMPROV_DIR=${PWD}
 
-roslaunch turtlebot_world.launch &
-sleep 15s
+# to switch simulators, should only need to change the SIM variable
+export SIM=$GAZEBO
+
+roslaunch $SIM &
+if [ "$SIM" = "$GAZEBO" ]; then
+    sleep 15s
+else
+    sleep 2s
+fi
 
 ./monitor ./user_instructions.txt ./mkDance
